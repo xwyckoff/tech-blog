1ade4070d0bac47bbfa0936052992e27073bed85
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-04-30 23:13:56 -0400

    set up session

--- server.js
+++ server.js
@@ -4,9 +4,24 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const session = require('express-session');
 const routes = require('./controllers');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//set up session
+app.use(session({
+    secret: 'supersecretsecret',
+    cookie: {
+        //make cookie expire after one day
+        maxAge: 24 * 60 * 60 * 1000
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: db
+    })
+}))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
